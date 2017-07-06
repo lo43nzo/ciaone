002e5f9464988d36dbf5126ab4478f30f43ae8d6
lorenzo <lorenzo@lorenzo-VirtualBox>
2017-07-06 16:55:07 +0200

    gitaven3 commit 1

---
+++ gitaven/src/main/java/gitaven/Ciaone.java
@@ -0,0 +1,7 @@
+package gitaven;
+
+public class Ciaone {
+	public String getString() {
+		return "Ciaone";
+	}
+}
---
+++ gitaven/src/main/java/gitaven/Main.java
@@ -0,0 +1,10 @@
+package gitaven;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Ciaone c=new Ciaone();
+		System.out.println(c.getString());
+	}
+
+}
